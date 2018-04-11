0dfaf2327c09810b464b14589587b8abf63312a2
Arnaud Kubler <arnaud.kubler@capgemini.com>
2018-04-11 16:46:00 +0200

    connect authentification to backend

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,14 +1,18 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 
+
 @Injectable()
 export class AuthenticationService {
+    apiUrl = environment.apiUrl;
     constructor(private http: HttpClient) { }
 
     login(username: string, password: string) {
-        return this.http.post<any>('/api/authenticate', { username: username, password: password })
+        return this.http.post<any>(this.apiUrl + '/Users/authenticate', { username: username, password: password })
             .map(user => {
                 // login successful if there's a jwt token in the response
                 if (user && user.token) {
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,28 +2,30 @@
 import { HttpClient } from '@angular/common/http';
 
 import { User } from '../_models/index';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class UserService {
+    apiUrl = environment.apiUrl;
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>('/api/users');
+        return this.http.get<User[]>(this.apiUrl + '/Users');
     }
 
     getById(id: number) {
-        return this.http.get('/api/users/' + id);
+        return this.http.get(this.apiUrl + '/Users/' + id);
     }
 
     create(user: User) {
-        return this.http.post('/api/users', user);
+        return this.http.post(this.apiUrl + '/Users', user);
     }
 
     update(user: User) {
-        return this.http.put('/api/users/' + user.id, user);
+        return this.http.put(this.apiUrl + '/Users/' + user.id, user);
     }
 
     delete(id: number) {
-        return this.http.delete('/api/users/' + id);
+        return this.http.delete(this.apiUrl + '/Users/' + id);
     }
 }
