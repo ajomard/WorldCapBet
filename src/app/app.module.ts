b8a2dcc1da4687be7b1cdff48801123ffd099459
Arnaud Kubler <arnaud.kubler@gmail.com>
2018-04-29 09:25:50 +0200

    maj inscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -44,6 +44,7 @@ import {MatDialogModule} from '@angular/material/dialog';
         NgbModule.forRoot(),
         NgxSpinnerModule,
         BrowserAnimationsModule,
+        ReactiveFormsModule,
         MatButtonModule,
         MatSidenavModule,
         MatToolbarModule,
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,17 +1,22 @@
 ﻿import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, Validators} from '@angular/forms';
 
 import { AlertService, UserService } from '../_services/index';
 
 @Component({
     moduleId: module.id.toString(),
-    templateUrl: 'register.component.html'
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 
 export class RegisterComponent {
     model: any = {};
     loading = false;
     passwordverification: "";
+
+    email = new FormControl('', [Validators.required, Validators.email]);
+    passwordVerificationControl = new FormControl('', [Validators.required]);
 
     constructor(
         private router: Router,
@@ -31,4 +36,13 @@ export class RegisterComponent {
                     this.loading = false;
                 });
     }
+
+    getErrorEmailMessage() {
+     return this.email.hasError('required') ? 'You must enter a value' :
+         this.email.hasError('email') ? 'Not a valid email' : '';
+   }
+
+   getErrorPasswordMessage() {
+     return 'test';
+   }
 }
