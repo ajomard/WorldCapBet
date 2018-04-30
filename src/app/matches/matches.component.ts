deb94f55e9530b3f8a83c7ae8d8867753537636d
Arnaud Kubler <arnaud.kubler@gmail.com>
2018-04-30 11:47:57 +0200

    ajout filtre matchs

--- src/app/matches/matches.component.ts
+++ src/app/matches/matches.component.ts
@@ -21,9 +21,9 @@ import {MatPaginator, MatSort, MatTableDataSource, MatDialog, MAT_DIALOG_DATA} f
 export class MatchesComponent implements OnInit {
   displayedColumns = ['date', 'team1', 'team2', 'pronostic', 'score', 'action'];
   dataSource: MatTableDataSource<Matches>;
-  isLoadingResults = true;
+  isLoadingResults = false;
+  isFilterOn = true;
 
-  //@ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private matchesService: MatchesService,
@@ -41,36 +41,34 @@ export class MatchesComponent implements OnInit {
         data: match
       });
 
-      dialogRef.afterClosed().subscribe(result => {
+      /*dialogRef.afterClosed().subscribe(result => {
       //  if(result != null)
           //match.pronostic = result;
-      });
+      });*/
     }
 
-    applyFilter(filterValue: string) {
-      filterValue = filterValue.trim(); // Remove whitespace
-      filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
-      this.dataSource.filter = filterValue;
+    changeFilter() {
+      this.isFilterOn = !this.isFilterOn;
+      this.getAllMatchesAndPronostics();
     }
 
   getAllMatchesAndPronostics() {
+    this.isLoadingResults = true;
     let userid = this.authenticationService.getLoggedUser().id;
     this.userService.getAllMatchesAndPronostics(userid).subscribe(matches => {
-      //this.matches = matches
-      this.dataSource = new MatTableDataSource(matches);
-      //this.dataSource.paginator = this.paginator;
+      let matchsResults = [];
+      if(this.isFilterOn) {
+        for(let match of matches) {
+          if(!this.isMatchAlreadyPlayed(match)) matchsResults.push(match);
+        }
+      } else {
+        matchsResults = matches;
+      }
+      this.dataSource = new MatTableDataSource(matchsResults);
       this.dataSource.sort = this.sort;
       this.isLoadingResults = false;
-      //this.sortByDate();
     });
   }
-
-  /*getPronostics() {
-    let userid = this.authenticationService.getLoggedUser().id;
-    this.userService.getPronostics(userid).subscribe(pronostics => this.pronostics = pronostics);
-  }*/
-
-
 
   isMatchAlreadyPlayed(match:Matches): boolean {
     return moment(match.date) <= moment();
@@ -84,21 +82,18 @@ export class MatchesComponent implements OnInit {
     return match.scoreTeam1 != null && match.scoreTeam2 != null;
   }
 
-  /*sortByDate(): void {
-    this.matches = this.matches.sort((a: Matches, b: Matches) => {
-        return moment(a.date).valueOf() - moment(b.date).valueOf();
-    })
-  }*/
+  filter() {
 
-  delete(match:Matches) {
+  }
+
+  /*delete(match:Matches) {
     this.matchesService.delete(match).subscribe(
         data => {
-
             this.alertService.success('Match Deleted');
         },
         error => {
             this.alertService.error(error.error);
         });;
-  }
+  }*/
 
 }
