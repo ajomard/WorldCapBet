d1607b1f7f2494a4de2c0cd1d45d1352dcbd151b
Arnaud Kubler <arnaud.kubler@gmail.com>
2018-05-10 21:25:20 +0200

    remove some element for mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://worldcapbetservice.azurewebsites.net/api/",
+  apiUrl: "https://worldcapbetservice.azurewebsites.net/api/",
   baseHrefForImages: "./"
 };
