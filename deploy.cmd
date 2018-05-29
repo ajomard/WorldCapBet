93c0f37fb845f732dc9b6a64d8bc4c9fcc3cec5f
ajomard <arnaud.jomard@capgemini.com>
2018-05-29 17:17:00 +0200

    update deployment file for azure (attemp 2)

--- deploy.cmd
+++ deploy.cmd
@@ -103,9 +103,9 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
-call :ExecuteCmd !NPM_CMD! run build
+:: call :ExecuteCmd !NPM_CMD! run build
 :: If the above command fails comment above and uncomment below one
-:: call ./node_modules/.bin/ng build –prod
+call ./node_modules/.bin/ng build –-prod
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
 )
