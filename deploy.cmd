23daf7ed55921d20bda153db34bb5ed0de0212b2
ajomard <arnaud.jomard@capgemini.com>
2018-06-04 11:39:31 +0200

    Removing only dev for npm install deployment

--- deploy.cmd
+++ deploy.cmd
@@ -94,7 +94,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   pushd "%DEPLOYMENT_SOURCE%"
-  call :ExecuteCmd !NPM_CMD! install --only=dev
+  call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
