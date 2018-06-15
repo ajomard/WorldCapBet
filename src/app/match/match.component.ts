59368bf0dd3fc19ee25afa49c703368353365b96
ajomard <arnaud.jomard@capgemini.com>
2018-06-15 15:19:13 +0200

    adding stats on match detail

--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -20,6 +20,14 @@ export class MatchComponent implements OnInit {
   dataSource: MatTableDataSource<Pronostic>;
   isLoadingResults = false;
   @ViewChild(MatSort) sort: MatSort;
+  totalProno;
+  victoryTeam1;
+  victoryTeam2;
+  draw;
+  score4;
+  score2;
+  score1;
+  score0;
 
   constructor(
     private route: ActivatedRoute,
@@ -57,29 +65,58 @@ export class MatchComponent implements OnInit {
           switch (property) {
             case 'firstName': return item.user.firstName;
             case 'name': return item.user.lastName;
+            case 'score' : return this.isRight(item);
             default: return item[property];
           }
         };
         this.dataSource.sort = this.sort;
         this.isLoadingResults = false;
+        this.getPronosStats();
       }
     );
   }
 
-  isRight(prono: Pronostic): boolean {
-    let res = false;
+  isRight(prono: Pronostic): number {
+    let res = 0;
     if (this.match.scoreTeam1 != null && this.match.scoreTeam2 != null) {
       const diff = this.match.scoreTeam1 - this.match.scoreTeam2;
       const diffProno = prono.scoreTeam1 - prono.scoreTeam2;
-
-      if (diff > 0 && diffProno > 0 || diff < 0 && diffProno < 0 || diff === 0 && diffProno === 0){
-        res = true;
+      if (prono.scoreTeam1 === this.match.scoreTeam1 && prono.scoreTeam2 === this.match.scoreTeam2) {
+        return 4;
+      } else if (diff === diffProno) {
+        return 2;
+      } else if (diff > 0 && diffProno > 0 || diff < 0 && diffProno < 0) {
+        res = 1;
       }
 
     } else {
-      res = false;
+      res = 0;
     }
     return res;
+  }
+
+  getPronosStats(): void {
+    this.totalProno = this.dataSource.data.length;
+    this.victoryTeam1 = (this.dataSource.data.filter(x => x.scoreTeam1 > x.scoreTeam2).length / this.totalProno * 100.0).toFixed(1);
+    this.victoryTeam2 = (this.dataSource.data.filter(x => x.scoreTeam2 > x.scoreTeam1).length / this.totalProno * 100.0).toFixed(1);
+    this.draw = (this.dataSource.data.filter(x => x.scoreTeam1 === x.scoreTeam2).length / this.totalProno * 100.0).toFixed(1);
+
+    this.score4 = (this.dataSource.data.
+      filter(x => this.isRight(x) === 4).length / this.totalProno * 100.0
+    ).toFixed(1);
+
+    this.score2 = (this.dataSource.data.
+      filter(x => this.isRight(x) === 2).length / this.totalProno * 100.0
+    ).toFixed(1);
+
+    this.score1 = (this.dataSource.data.
+      filter(x => this.isRight(x) === 1).length / this.totalProno * 100.0
+    ).toFixed(1);
+
+    this.score0 = (this.dataSource.data.
+      filter(x => this.isRight(x) === 0).length / this.totalProno * 100.0
+    ).toFixed(1);
+
   }
 
 }
